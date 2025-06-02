35e5de7d1dbc890bfad3ca095a798ca9d5699922
k.i.r.a. <kiravillagracia@yahoo.com>
2025-06-02 11:34:04 +0800

    initial commit

---
+++ src/test/kotlin/com/kira/api/NotesAPI/NotesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kira.api.NotesAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotesApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
